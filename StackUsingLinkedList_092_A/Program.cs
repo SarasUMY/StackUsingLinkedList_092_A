1c528caa08d57adcd53917131eae1c5c71b01cb2
TA1092 <bagas.saras.ft21@mail.umy.ac.id>
2022-11-30 17:10:20 +0700

    Add method pop

--- StackUsingLinkedList_092_A/Program.cs
+++ StackUsingLinkedList_092_A/Program.cs
@@ -41,6 +41,10 @@ namespace StackUsingLinkedList_092_A
             top = fresh;
             Console.WriteLine("\n" + element + "pushed.");
         }
-
+        public void pop()
+        {
+            Console.WriteLine("\nThe popped element is: " + top.info);
+            top = top.next; //Make top point to the next node in sequence
+        }
     }
 }
